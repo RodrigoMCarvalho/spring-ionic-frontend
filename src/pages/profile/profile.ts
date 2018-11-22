8a36379d69cbdc4897cfa3de8eb5f9b3557fc7bd
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-11-22 21:30:01 -0200

    tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,3 +1,4 @@
+import { API_CONFIG } from './../../config/api.config';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
@@ -26,11 +27,26 @@ export class ProfilePage {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
           this.cliente = response;
+          this.getImageExists();
         },
-        error => {});
-
-      console.log(localUser.email);
+        error => {
+          if(error.status == 403) { //se o erro for 403 direciona para o home
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage'); //caso tenha problema para obter o localUser
     }
   }
 
-}
+  getImageExists() {
+    this.clienteService.getImageFromBucket(this.cliente.id)
+      .subscribe(response => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      },
+        error => {});
+    }
+  }
+
+
