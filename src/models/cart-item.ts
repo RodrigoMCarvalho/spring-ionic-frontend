a4c98c19f03a44896ae6826267f0e8b4ba27adbf
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-12-07 23:19:37 -0200

    comecando a criar pagina de carrinho de compras

---
+++ src/models/cart-item.ts
@@ -0,0 +1,6 @@
+
+
+export interface CartItem{
+    quantidade: number,
+    produto: ProdutoDTO
+}
---
+++ src/services/domain/cart.service.ts
@@ -0,0 +1,35 @@
+import { StorageService } from './../storage.service';
+import { Injectable } from '@angular/core';
+import { Cart } from '../../models/cart';
+import { ProdutoDTO } from '../../models/produto.dto';
+
+@Injectable()
+export class CartService{
+
+  constructor(public storage: StorageService){
+  }
+
+  createOrClearCart(): Cart {
+    let cart: Cart = {item:[]};  //cria um carrinho vazio
+    this.storage.setCart(cart);  //armazena ele no localStorage
+    return cart; //retorna o cart
+  }
+
+  getCart(): Cart {
+    let cart: Cart = this.storage.getCart();
+    if(cart == null){
+      cart = this.createOrClearCart();  // se o cart não existir, deverá ser criado
+    }
+    return cart; //retorna um cart novo ou o armazenado no storage
+  }
+
+  addProduto(produto: ProdutoDTO) : Cart {
+    let cart = this.getCart();  // trata se o carrinho existe
+    let position = cart.item.findIndex(x => x.produto.id == produto.id)    //encontrar a posição de um elemento
+    if(position == -1) {  //findIndex retorna -1 se o produto nao existir
+      cart.item.push({quantidade: 1, produto: produto});
+    }
+    this.storage.setCart(cart);
+    return cart;
+  }
+}
