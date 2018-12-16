3b397e1cd22c9857d01b1bcae1efcef659c22de6
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-12-15 22:21:40 -0200

    finalizando tela de pagamento e confirmação de pedido

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -1,6 +1,7 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { PedidoDTO } from '../../models/pedido.dto';
 
 @IonicPage()
 @Component({
@@ -9,11 +10,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PaymentPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  pedido: PedidoDTO;
+  parcelas: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  formGroup: FormGroup; //formulário para controlar os dados de entrada
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuild: FormBuilder) {
+
+      this.pedido = this.navParams.get('pedido'); //obter o objeto "pedido" que vier da outra página
+      console.log(this.pedido);
+
+      this.formGroup = this.formBuild.group({
+        numeroDeParcelas: [1, Validators.required],
+        "@type": ["pagamentoComCartao", Validators.required] //palavra que identifica a classe no json
+      })
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PaymentPage');
+  nextPage() {
+    this.pedido.pagamento = this.formGroup.value;
+    //console.log(this.pedido);
+    this.navCtrl.setRoot('OrderConfirmationPage', { pedido: this.pedido });
   }
 
 }
