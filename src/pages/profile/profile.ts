f4803146f3cb1283340ca414b6851942b0605b8a
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-12-29 23:09:21 -0200

    fazendo upload de imagens

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,6 +26,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -66,8 +70,23 @@ export class ProfilePage {
       this.camera.getPicture(options).then((imageData) => {
        this.picture = 'data:image/png;base64,' + imageData;
        this.cameraOn = false;
-      }, (err) => {
+      },
+        (err) => {
       });
+    }
+
+    sendPicture() {
+      this.clienteService.uploadPicture(this.picture)
+        .subscribe(response => {
+          this.picture = null;
+          this.loadData();
+        },
+          error => {
+        });
+    }
+
+    cancel() {
+      this.picture = null;
     }
   }
 
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,3 +1,4 @@
+import { ImageUtilService } from './../image-util.service';
 import { StorageService } from './../storage.service';
 import { API_CONFIG } from './../../config/api.config';
 import { Observable } from 'rxjs/Rx';
@@ -11,7 +12,8 @@ export class ClienteService {
 
     constructor(
       public http : HttpClient,
-      public storage : StorageService){
+      public storage : StorageService,
+      public imageUtilService : ImageUtilService){
     }
 
     findById(id : string) {
@@ -24,8 +26,20 @@ export class ClienteService {
 
     getImageFromBucket(id: string) : Observable<any> {
       let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-
       return this.http.get(url, {responseType : 'blob'});
+    }
+
+    uploadPicture(picture ) {
+      let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+      let formData: FormData = new FormData();
+      formData.set('file', pictureBlob, 'file.png');
+      return this.http.post(
+        `${API_CONFIG.baseUrl}/clientes/picture`,
+        formData,                                //enviar o objeto formData
+        {
+          observe: 'response',
+          responseType: 'text' //para evitar um erro de parse em JSON num corpo vazio
+      });
     }
 
     insert(obj: ClienteDTO) {
